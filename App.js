23725af69343b2a6e1bdfafadaceea939fbc8885
Rizkysyam <rizkysyam19@gmail.com>
2023-06-17 02:14:19 +0700

    page aman

--- App.js
+++ App.js
@@ -1,61 +1,217 @@
-import React from 'react';
-import { View, TextInput, TouchableOpacity, Text, Image, Dimensions, ScrollView } from 'react-native';
-import { IconButton, Card, LeftContent, Button } from 'react-native-paper';
-
+import * as React from 'react';
+import { View, ScrollView, ImageBackground, StyleSheet, Image } from 'react-native';
+import { Searchbar, Avatar, IconButton, Text, Divider, List, BottomNavigation } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
-    return (
-        <View>
-            <View style={{
-                backgroundColor: '#6E2625', flexDirection: 'row', height: 75, borderBottomLeftRadius: 20,
-                borderBottomRightRadius: 20, justifyContent: 'space-between', alignItems: 'center'
-            }}>
-                <IconButton
-                    icon="magnify"
-                    iconColor='black'
-                    containerColor='white'
-                    size={24}
-                    onPress={() => console.log('Pressed')}
-                />
 
-                <Text style={{ color: 'white', fontSize: 24, fontWeight: 'bold' }}>
-                    PawMatch
-                </Text>
+  const [searchQuery, setSearchQuery] = React.useState('');
 
-                <IconButton
-                    icon="bell-outline"
-                    iconColor='black'
-                    containerColor='white'
-                    size={24}
-                    onPress={() => console.log('Pressed')}
-                />
-            </View>
-            <View style={{flexDirection: 'row',justifyContent: 'center',padding : 20}}>
-                <View>
-                <Card>
-                    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-                    <Card.Title title="Card Title" subtitle="Card Subtitle"/>
-                    <Card.Content>
-                        <Text variant="titleLarge">Card title</Text>
-                        <Text variant="bodyMedium">Card content</Text>
-                    </Card.Content>
-                </Card>
-                </View>
+  // const MusicRoute = () => <Text>Music</Text>;
 
-                <View>
-                <Card>
-                    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-                    <Card.Title title="Card Title" subtitle="Card Subtitle" />
-                    <Card.Content>
-                        <Text variant="titleLarge">Card title</Text>
-                        <Text variant="bodyMedium">Card content</Text>
-                    </Card.Content>
-                </Card>
-                </View>
+  // const AlbumsRoute = () => <Text>Albums</Text>;
+  
+  // const RecentsRoute = () => <Text>Recents</Text>;
+  
+  // const NotificationsRoute = () => <Text>Notifications</Text>;
+  // const [index, setIndex] = React.useState(0);
+  // const [routes] = React.useState([
+  //   { key: 'music', title: 'Favorites', focusedIcon: 'heart', unfocusedIcon: 'heart-outline'},
+  //   { key: 'albums', title: 'Albums', focusedIcon: 'album' },
+  //   { key: 'recents', title: 'Recents', focusedIcon: 'history' },
+  //   { key: 'notifications', title: 'Notifications', focusedIcon: 'bell', unfocusedIcon: 'bell-outline' },
+  // ]);
 
-            </View>
+  // const renderScene = BottomNavigation.SceneMap({
+  //   music: MusicRoute,
+  //   albums: AlbumsRoute,
+  //   recents: RecentsRoute,
+  //   notifications: NotificationsRoute,
+  // });
+
+  return (
+    <ScrollView>
+      <View style={{ flexDirection: "row" }}>
+        <Avatar.Image
+          size={40}
+          source={require('./assets/images/logo.png')}
+          style={{ marginVertical: 20, marginLeft: 20, marginRight: 10, alignSelf: "center", backgroundColor: "transparent" }} />
+        <Searchbar
+          style={{ flex: 1, marginVertical: 20, marginLeft: 10, marginRight: 5, height: 45, alignSelf: "center" }}
+          placeholder="Cari wisata"
+          onChangeText={value => { setSearchQuery(value) }}
+          value={searchQuery}
+          inputStyle={{ fontWeight: "300", opacity: searchQuery == '' ? 0.5 : 1, alignSelf: "center" }} />
+        <IconButton
+          style={{ marginVertical: 20, marginRight: 10, alignSelf: "center" }}
+          icon="heart-outline"
+          iconColor={"#0f0f0f"}
+          size={25}
+          onPress={() => console.log('Pressed')} />
+      </View>
+      <View style={styles.container}>
+        <ImageBackground source={require('./assets/images/PulauBukulimauUnderwater.png')} resizeMode="cover" style={styles.image1}>
+          <Text style={styles.text1}>Wisata Air</Text>
+          <Text style={styles.text2}>Pulau Bukulimau Underwater</Text>
+        </ImageBackground>
+      </View>
+      <View>
+        <Text style={styles.text3}>Destinasi Wisata</Text>
+        <Text style={styles.text4}>Pilihan terbaik</Text>
+      </View>
+      <View style={{ flexDirection: "row", flexWrap: "wrap", rowGap: 20, columnGap: 20, marginHorizontal: 20 }}>
+        <ImageBackground source={require('./assets/images/pantai-serdang.png')} resizeMode="cover" style={styles.image2} imageStyle={{ borderRadius: 15 }}>
+          <Text style={styles.text5}>Pantai Serdang</Text>
+        </ImageBackground>
+        <ImageBackground source={require('./assets/images/vihara-patung-dewi-kwan-im.png')} resizeMode="cover" style={styles.image2} imageStyle={{ borderRadius: 15 }}>
+          <Text style={styles.text5}>Vihara Patung Dewi Kwan Im</Text>
+        </ImageBackground>
+        <ImageBackground source={require('./assets/images/replika-sd-laskar-pelangi.png')} resizeMode="cover" style={styles.image2} imageStyle={{ borderRadius: 15 }}>
+          <Text style={styles.text5}>Replika SD Laskar Pelangi</Text>
+        </ImageBackground>
+        <ImageBackground source={require('./assets/images/pantai-nyiur-melambai.png')} resizeMode="cover" style={styles.image2} imageStyle={{ borderRadius: 15 }}>
+          <Text style={styles.text5}>Pantai Nyiur Melambai</Text>
+        </ImageBackground>
+      </View>
+      <Text style={{ color: '#0085CC', alignSelf: 'center', marginTop: 30, marginBottom: 50, fontWeight: "700" }}>Lihat lainnya</Text>
+      <Divider bold={true} style={{ marginHorizontal: 20 }}></Divider>
+      <View>
+        <Text style={styles.text3}>Jelajahi Sekarang</Text>
+        <Text style={styles.text4}>Pilih kategori yang diminati</Text>
+      </View>
+      <View style={{ marginBottom: 30 }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginHorizontal: 20, marginBottom: 20 }}>
+          <View>
+            <Image style={styles.categoricalIcon} source={require('./assets/icons/categorical/wisata-alam.png')} />
+            <Text style={{ fontSize: 10, textAlign: 'center', maxWidth: 50, marginVertical: 5 }}>Wisata Alam</Text>
+          </View>
+          <View>
+            <Image style={styles.categoricalIcon} source={require('./assets/icons/categorical/wisata-air.png')} />
+            <Text style={{ fontSize: 10, textAlign: 'center', maxWidth: 50, marginVertical: 5 }}>Wisata Air</Text>
+          </View>
+          <View>
+            <Image style={styles.categoricalIcon} source={require('./assets/icons/categorical/wisata-kuliner.png')} />
+            <Text style={{ fontSize: 10, textAlign: 'center', maxWidth: 50, marginVertical: 5 }}>Wisata Kuliner</Text>
+          </View>
+          <View>
+            <Image style={styles.categoricalIcon} source={require('./assets/icons/categorical/wisata-sejarah.png')} />
+            <Text style={{ fontSize: 10, textAlign: 'center', maxWidth: 50, marginVertical: 5 }}>Wisata Sejarah</Text>
+          </View>
         </View>
-    );
+        <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginHorizontal: 20 }}>
+          <View>
+            <Image style={styles.categoricalIcon} source={require('./assets/icons/categorical/hotel-penginapan.png')} />
+            <Text style={{ fontSize: 10, textAlign: 'center', maxWidth: 50, marginVertical: 5 }}>Wisata Sejarah</Text>
+          </View>
+          <View>
+            <Image style={styles.categoricalIcon} source={require('./assets/icons/categorical/layanan-publik.png')} />
+            <Text style={{ fontSize: 10, textAlign: 'center', maxWidth: 50, marginVertical: 5 }}>Wisata Sejarah</Text>
+          </View>
+          <View>
+            <Image style={styles.categoricalIcon} source={require('./assets/icons/categorical/travel-transportasi.png')} />
+            <Text style={{ fontSize: 10, textAlign: 'center', maxWidth: 50, marginVertical: 5 }}>Wisata Sejarah</Text>
+          </View>
+          <View>
+            <Image style={styles.categoricalIcon} source={require('./assets/icons/categorical/belanja-oleh-oleh.png')} />
+            <Text style={{ fontSize: 10, textAlign: 'center', maxWidth: 50, marginVertical: 5 }}>Wisata Sejarah</Text>
+          </View>
+        </View>
+      </View>
+      <View>
+        <Image style={{ justifyContent: 'flex-start', width: '80%', alignSelf: 'center' }} source={require('./assets/images/kopit19.png')} resizeMode='contain' />
+      </View>
+      <View>
+        <Text style={styles.text3}>Informasi dan Berita</Text>
+        <Text style={styles.text4}>Seputar Belitung Timur</Text>
+      </View>
+      <View style={{ marginHorizontal: 20 }}>
+        <Divider bold={true}></Divider>
+        <List.Item
+          // titleStyle={{justifyContent: 'flex-start'}}
+          title="Adakan Kerjurkab Tinju 2022"
+          description="20 Oktober 2021"
+          left={() => <Image source={require('./assets/images/kerjurkab.png')} />} />
+        <Divider bold={true}></Divider>
+        <List.Item
+          // titleStyle={{justifyContent: 'flex-start'}}
+          title="Wabup Beltim Apresiasi Job Fair Beltim"
+          description="15 Oktober 2021"
+          left={() => <Image source={require('./assets/images/jobfair.png')} />} />
+        <Divider bold={true}></Divider>
+        <List.Item
+          // titleStyle={{justifyContent: 'flex-start'}}
+          title="LKPM Buat Proyek Pemerintah Jadi Lebih Terpantau"
+          description="20 Oktober 2021"
+          left={() => <Image source={require('./assets/images/lkpm.png')} />} />
+        <Divider bold={true}></Divider>
+        <Text style={{ color: '#0085CC', alignSelf: 'center', marginTop: 30, marginBottom: 50, fontWeight: "700" }}>Informasi lainnya</Text>
+      </View>
+      
+      {/* <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    /> */}
+    </ScrollView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image1: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    height: 300
+  },
+  image2: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    height: 200,
+    minWidth: '40%'
+  },
+  categoricalIcon: {
+    // minWidth: '10%'
+    alignSelf: 'center',
+
+  },
+  text1: {
+    marginLeft: 20,
+    color: 'white',
+    fontSize: 14,
+    textAlign: 'left',
+    opacity: 0.8
+  },
+  text2: {
+    marginLeft: 20,
+    marginBottom: 20,
+    color: 'white',
+    fontSize: 32,
+    fontWeight: "700",
+    textAlign: 'left',
+  },
+  text3: {
+    marginTop: 30,
+    color: '#0f0f0f',
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  text4: {
+    marginTop: 3,
+    marginBottom: 30,
+    color: '#0f0f0f',
+    fontSize: 12,
+    textAlign: 'center',
+    opacity: 0.6
+  },
+  text5: {
+    margin: 15,
+    color: '#f0f0f0',
+    fontSize: 14,
+    fontWeight: "700",
+    textAlign: 'left',
+  },
+});
 
 export default App;
--- src/Pages/homePage/index.js
+++ src/Pages/homePage/index.js
@@ -1,61 +1,168 @@
 import React from 'react';
-import { View, TextInput, TouchableOpacity, Text, Image, Dimensions, ScrollView } from 'react-native';
-import { IconButton, Card, LeftContent, Button } from 'react-native-paper';
+import { View, TextInput, TouchableOpacity, Image, Dimensions, ScrollView } from 'react-native';
+import { IconButton, Card, LeftContent, Button, Text } from 'react-native-paper';
+
+const { width } = Dimensions.get('window');
 
 
 const App = () => {
-    return (
-        <View>
-            <View style={{
-                backgroundColor: '#6E2625', flexDirection: 'row', height: 75, borderBottomLeftRadius: 20,
-                borderBottomRightRadius: 20, justifyContent: 'space-between', alignItems: 'center'
-            }}>
-                <IconButton
-                    icon="magnify"
-                    iconColor='black'
-                    containerColor='white'
-                    size={24}
-                    onPress={() => console.log('Pressed')}
-                />
+  return (
+    <ScrollView>
 
-                <Text style={{ color: 'white', fontSize: 24, fontWeight: 'bold' }}>
-                    PawMatch
-                </Text>
+      <View style={{
+        backgroundColor: '#6E2625', flexDirection: 'row', height: 75, borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20, justifyContent: 'space-between', alignItems: 'center'
+      }}>
+        <IconButton
+          icon="magnify"
+          iconColor='black'
+          containerColor='white'
+          size={24}
+          onPress={() => console.log('Pressed')}
+        />
 
-                <IconButton
-                    icon="bell-outline"
-                    iconColor='black'
-                    containerColor='white'
-                    size={24}
-                    onPress={() => console.log('Pressed')}
-                />
-            </View>
-            <View style={{flexDirection: 'row',justifyContent: 'center',padding : 20}}>
-                <View>
-                <Card>
-                    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-                    <Card.Title title="Card Title" subtitle="Card Subtitle"/>
-                    <Card.Content>
-                        <Text variant="titleLarge">Card title</Text>
-                        <Text variant="bodyMedium">Card content</Text>
-                    </Card.Content>
-                </Card>
-                </View>
+        <Text style={{ color: 'white', fontSize: 24, fontWeight: 'bold' }}>
+          PawMatch
+        </Text>
 
-                <View>
-                <Card>
-                    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-                    <Card.Title title="Card Title" subtitle="Card Subtitle" />
-                    <Card.Content>
-                        <Text variant="titleLarge">Card title</Text>
-                        <Text variant="bodyMedium">Card content</Text>
-                    </Card.Content>
-                </Card>
-                </View>
-
-            </View>
+        <IconButton
+          icon="bell-outline"
+          iconColor='black'
+          containerColor='white'
+          size={24}
+          onPress={() => console.log('Pressed')}
+        />
+      </View>
+        <View style={{alignSelf:'center'}}>
+        <Image
+        source={require('../../assets/image/card2.png')}
+        />
         </View>
-    );
+      <View>
+      <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between',marginVertical:10}}>
+      <Text variant="headlineSmall" style={{ paddingLeft : 10}}>Kucing</Text>
+      <Text variant="labelLarge" style={{paddingRight: 15, color: '#6E2625'}}>Lihat Semua</Text>
+      </View>
+      <View style={{ flexDirection: "row", flexWrap: "wrap", justifyContent: "space-between", paddingHorizontal: 10 }}>
+        <Card style={{ width: width * 0.45 , marginBottom:20 , backgroundColor :'white'}}>
+          <Card.Cover source={require('../../assets/image/cat1.jpg')} />
+          <Card.Content>
+            <Text variant="titleLarge">Yanto</Text>
+            <Text variant="labelLarge">Scottish Fold</Text>
+            <Text variant="labelLarge">2.5 Tahun</Text>
+            <Text variant="labelMedium">Bandung</Text>
+          </Card.Content>
+          <Card.Actions style={{ justifyContent: 'flex-end' }}>
+            <IconButton
+              mode= 'contained'
+              icon="cat"
+              size={20}
+              iconColor='#6E2625'
+
+            />
+            <IconButton
+            mode='default'
+              icon="gender-male"
+              size={20}
+              iconColor='blue'
+              containerColor='#E1FFFD'
+            />
+          </Card.Actions>
+        </Card>
+        <Card style={{ width: width * 0.45 , marginBottom:20 , backgroundColor :'white',}}>
+          <Card.Cover source={require('../../assets/image/cat2.jpg')} />
+          <Card.Content>
+            <Text variant="titleLarge">Yanti</Text>
+            <Text variant="labelLarge">Anggora</Text>
+            <Text variant="labelLarge">1 Tahun</Text>
+            <Text variant="labelMedium">Bandung</Text>
+          </Card.Content>
+          <Card.Actions style={{ justifyContent: 'flex-end' }}>
+            <IconButton
+              mode= 'contained'
+              icon="cat"
+              size={20}
+              iconColor='#6E2625'
+
+            />
+            <IconButton
+              mode='default'
+              icon="gender-female"
+              size={20}
+              iconColor='#FFB6C1'
+              containerColor='#FFE9FF'
+            />
+          </Card.Actions>
+        </Card>
+        </View>
+       
+      <View>
+      <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between',marginVertical:10}}>
+      <Text variant="headlineSmall" style={{ paddingLeft : 10}}>Anjing</Text>
+      <Text variant="labelLarge" style={{paddingRight: 15, color: '#6E2625'}}>Lihat Semua</Text>
+      </View>
+      <View style={{ flexDirection: "row", flexWrap: "wrap", justifyContent: "space-between", paddingHorizontal: 10 }}>
+        <Card style={{ width: width * 0.45 , marginBottom:20 , backgroundColor :'white'}}>
+          <Card.Cover source={require('../../assets/image/dog1.jpeg')} />
+          <Card.Content>
+            <Text variant="titleLarge">Roti Tawar</Text>
+            <Text variant="labelLarge">Golden</Text>
+            <Text variant="labelLarge">3.5 Tahun</Text>
+            <Text variant="labelMedium">Cimahi</Text>
+          </Card.Content>
+          <Card.Actions style={{ justifyContent: 'flex-end' }}>
+            <IconButton
+              mode= 'contained'
+              icon="dog"
+              size={20}
+              iconColor='#6E2625'
+
+            />
+            <IconButton
+              mode='default'
+              icon="gender-male"
+              size={20}
+              iconColor='blue'
+              containerColor='#E1FFFD'
+            />
+          </Card.Actions>
+        </Card>
+        <Card style={{ width: width * 0.45 , marginBottom:20 , backgroundColor :'white',}}>
+          <Card.Cover source={require('../../assets/image/dog2.jpg')} />
+          <Card.Content>
+            <Text variant="titleLarge">Siti</Text>
+            <Text variant="labelLarge">Pudel</Text>
+            <Text variant="labelLarge">2 Tahun</Text>
+            <Text variant="labelMedium">Garut</Text>
+          </Card.Content>
+          <Card.Actions style={{ justifyContent: 'flex-end' }}>
+            <IconButton
+              mode= 'contained'
+              icon="dog"
+              size={20}
+              iconColor='#6E2625'
+              
+
+            />
+            <IconButton
+              mode='default'
+              icon="gender-female"
+              size={20}
+              iconColor='#FFB6C1'
+              containerColor='#FFE9FF'
+            />
+          </Card.Actions>
+        </Card>
+        </View>
+      </View>
+      </View>
+        <View style={{alignSelf:'center'}}>
+        <Image
+        source={require('../../assets/image/card1.png')}
+        />
+        </View>
+    </ScrollView>
+  );
 };
 
 export default App;
